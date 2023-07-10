2bda537d233ed1b2311ae18894452126ddc89903
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-10 06:44:04 +0500

    Add a method to BookList class that compiles the content of the list section

--- modules/BookList.js
+++ modules/BookList.js
@@ -13,6 +13,7 @@ class BookList {
         const book = { title, author };
         console.log("I'm adding a book", book);
         this.addBook(book);
+        this.compileBookList();
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
       } 
@@ -23,6 +24,20 @@ class BookList {
     this.books.push(book);
     localStorage.setItem('books', JSON.stringify(this.books));
   }
-}
+
+  compileBookList() {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const li = document.createElement('li');
+      li.textContent = `"${book.title}" by ${book.author}`;
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      li.appendChild(removeBtn);
+      bookList.appendChild(li);
+    });
+  };
+};
 
 export { BookList };
