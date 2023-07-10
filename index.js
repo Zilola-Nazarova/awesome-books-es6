a7da3b9f92e29973e5c2800e35ade23c7daae8e0
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-10 05:59:41 +0500

    Add a module that adds an eventListener to button to create an object with book title and author and reset the input values, import and call this function in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { init } from "./modules/nav.js";
 import { updateDateTime } from "./modules/DateTime.js";
+import { addBtn } from './modules/AddBtn.js'
 init();
 updateDateTime();
+addBtn();
